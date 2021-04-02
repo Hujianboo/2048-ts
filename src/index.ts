d139323e553fef343c0e00d3454a4b0143da9555
hujianbo <466636709@qq.com>
2021-04-02 12:13:25 +0800

    feat: init

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: Hujianbo
+ * @Date: 2021-04-01 23:33:53
+ * @LastEditors: Hujianbo
+ * @LastEditTime: 2021-04-02 12:03:41
+ * @FilePath: /2048-ts/src/index.ts
+ */
+import '../css/index.scss'
+console.log(333);
+class grid {
+  size = 0
+}
